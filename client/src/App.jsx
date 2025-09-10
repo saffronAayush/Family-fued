4749b1231cefa0ac4a6aa3cddab1680ad18ac180
Aayush Soni <soniaayush5562@gmail.com>
2025-09-11 00:54:17 +0530

    family fued only fronent is here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import FamilyFeud from "./FamilyFeud";
 import "./App.css";
-
+// only to commit
 function App() {
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-purple-900 via-blue-900 to-black">
